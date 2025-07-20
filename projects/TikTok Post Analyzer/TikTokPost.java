a6c5d3c1f33a19bcb09f7620d0fe79f2c1198b15
ashton-mchale <30519802+ashton-mchale@users.noreply.github.com>
2025-07-20 00:03:03 -0400

    Fix constructor syntax and implement hashtag extraction

--- projects/TikTok Post Analyzer/TikTokPost.java
+++ projects/TikTok Post Analyzer/TikTokPost.java
@@ -10,13 +10,24 @@ public class TikTokPost {
     private List<String> comments;
 
     // Creation of the constructor
-    public TikTokPost(String caption, int views, int likes, int shares, List<String> comments {
+    public TikTokPost(String caption, int views, int likes, int shares, List<String> comments) {
         this.caption = caption;
         this.views = views;
         this.likes = likes;
         this.shares = shares;
         this.comments = comments;
         this.hashtags = extractHashtags(caption); // auto-extract hashtags
+    }
+
+    // Extract hashtags from the caption (words starting with '#')
+    private List<String> extractHashtags(String caption) {
+        List<String> tags = new ArrayList<>();
+        for (String word : caption.split("\\s+")) {
+            if (word.startsWith("#") && word.length() > 1) {
+                tags.add(word.substring(1));
+            }
+        }
+        return tags;
     }
     // TODO: Method for extracting hashtags from caption; Add getter methods
 }
